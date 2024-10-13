2c9b7430a64fb0e6ae7ca71117f7af303c17fcf6
Harm Matthias Harms <matthias.harms@quis.de>
2024-10-13 15:44:31 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { notify } from './slack'
 
-export async function run () {
+export async function run) {
   try {
     if (github.context.eventName !== 'release') {
       core.info('Action should only be run on release publish event!')
@@ -11,7 +11,7 @@ export async function run () {
 
     const repo = github.context.repo
     const release = github.context.payload.release
-    const slackWebhookUrl = core.getInput('slack_webhook_url')
+    const slackWebhookUrl = core.getInput('slack-webhook-url')
 
     await notify(slackWebhookUrl, release, repo)
     core.info('Slack notification sent.')
